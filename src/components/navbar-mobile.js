a25277ae180dd211dfd1b0b05a1e2df188b0650a
Duran_Ray <duran@teamgeek.co.za>
2018-11-13 11:33:19 +0200

    cleanup

--- src/components/navbar-mobile.js
+++ src/components/navbar-mobile.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 
-
-
 class MobileDropdown extends Component {
   render() {
     return (
