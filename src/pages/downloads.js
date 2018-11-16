e111fae27b0dbf62af5142d9b9e7a741dd4a61ff
Duran_Ray <duran@teamgeek.co.za>
2018-11-16 14:04:50 +0200

    added content

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react'
 import styled from 'styled-components';
-import { color } from '../utils/baseStyles';
+import { color, Button } from '../utils/baseStyles';
 
 import Banner from '../components/pages/banner';
 
+import htechDataSheet from '../../static/pdf/htech-product-data-sheet.pdf';
+import mtechDegreaserPDS from '../../static/pdf/mtech-degreaser-pds.pdf';
+import biodegradabilityCertificate from '../../static/pdf/biodegradability-certificate.pdf';
+import chromeGradeVsDosage from '../../static/pdf/chrome-grade-vs-dosage.pdf';
+import disfloPtTest from '../../static/pdf/disflo-pt-test.pdf';
+import htechFeasabilityReport from '../../static/pdf/htech-feasability-report.pdf';
+import htechHydrocarbonEliminator from '../../static/pdf/htech-hydrocarbon-eliminator-web.pdf';
+import lubritechBee from '../../static/pdf/lubritech-manufacturing-bee.pdf';
+import stdTest from '../../static/pdf/std-test.pdf';
+
+const FormContainer = styled.div`
+  text-align:center;
+  form {
+    .item {
+      label, input, textarea {
+        display: block;
+        margin: 0px auto;
+      }
+      input, textarea {
+        padding: 5px 10px;
+        border: 2px solid ${color.green};
+        border-radius: 20px;
+        outline: none;
+        width: 40%;
+      }
+      label {
+        padding-top: 30px;
+        font-weight: 700;
+      }
+      textarea {
+        min-height: 20vh;
+      }
+    }
+    button {
+      margin-top: 30px;
+    }
+  }
+`;
+
+const DownloadsContainer = styled.div`
+   padding: 30px;
+`;
+
 const encode = (data) => {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 class DownloadsPage extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      name: "", 
-      email: "", 
-      message: "" 
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      formSuccess: false
     };
   }
 
@@ -26,45 +70,91 @@ class DownloadsPage extends Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state })
     })
-      .then(() => alert("Success!"))
+      .then(() => {
+        this.setState({
+          formSuccess: true
+        })
+      })
       .catch(error => alert(error));
 
     e.preventDefault();
   };
 
   handleChange = (e) => {
-    this.setState({ 
-      [e.target.name]: e.target.value 
+    this.setState({
+      [e.target.name]: e.target.value
     });
   }
 
   render() {
     const { name, email, message } = this.state;
-    return(
+    return (
       <div>
-        <Banner title="Downloads"/>
+        <Banner title="Downloads" />
+        {!this.state.formSuccess &&
+          <FormContainer>
+            <form onSubmit={this.handleSubmit} name="contact" data-netlify="true" data-netlify-honeypot="bot">
+              <div className="item">
+                <label>Name</label>
+                <input type="text" name="name" value={name} onChange={this.handleChange} />
+              </div>
+              <div className="item">
+                <label>Email</label>
+                <input type="email" name="email" value={email} onChange={this.handleChange} />
+              </div>
+              <div className="item">
+                <label>Message</label>
+                <textarea name="message" value={message} onChange={this.handleChange} />
+              </div>
+              <Button type="submit">Send</Button>
+            </form>
+          </FormContainer>
+        }
 
-        <form onSubmit={this.handleSubmit} name="contact" data-netlify="true" data-netlify-honeypot="bot">
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-
+       {this.state.formSuccess && 
+         <DownloadsContainer>
+           <h3>SUCCESS</h3>
+           <div>
+              <div>
+                <h4>title</h4>
+                <a href={htechDataSheet} target="_blank">1</a>
+                <i className="fa fa-twitter"></i>
+              </div>
+              <div>
+                <h4>title</h4>
+                <a href={mtechDegreaserPDS} target="_blank"></a>
+              </div>
+              <div>
+                <h4>title</h4>
+                <a href={biodegradabilityCertificate} target="_blank"></a>
+              </div>
+              <div>
+                <h4>title</h4>
+                <a href={chromeGradeVsDosage} target="_blank"></a>
+              </div>
+              <div>
+                <h4>title</h4>
+                <a href={disfloPtTest} target="_blank"></a>
+              </div>
+              <div>
+                <h4>title</h4>
+                <a href={htechFeasabilityReport} target="_blank"></a>
+              </div>
+              <div>
+                <h4>title</h4>
+                <a href={htechHydrocarbonEliminator} target="_blank"></a>
+              </div>
+              <div>
+                <h4>title</h4>
+                <a href={lubritechBee} target="_blank"></a>
+              </div>
+              <div>
+                <h4>title</h4>
+                <a href={stdTest} target="_blank"></a>
+              </div>
+           </div>
+         </DownloadsContainer>
+      }
       </div>
     );
   }
