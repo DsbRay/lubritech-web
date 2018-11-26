d11b5e6e600937fb5009ff4a2dbb10df326e11bc
Duran_Ray <duran@teamgeek.co.za>
2018-11-26 11:26:05 +0200

    conditional rendering on gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import Banner from '../components/pages/banner';
-
+import styled from 'styled-components';
+import { color, Divider } from '../utils/baseStyles'
 import ImagesPage from '../components/pages/images';
 import VideosPage from '../components/pages/videos';
+
+const Headers = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  text-align: center;
+  padding-top: 30px;
+  h2 {
+    font-weight: 700;
+    transition: color 0.25s ease;
+    cursor: pointer;
+    &:hover {
+      color: ${color.green};
+    }
+  }
+`;
 
 class Gallery extends Component {
   constructor(props) {
@@ -12,13 +28,37 @@ class Gallery extends Component {
     }
   }
   render() {
-    return(
+    return (
       <div>
         <Banner title="Gallery" />
-        <ImagesPage />
-        <VideosPage />
+        <Headers>
+          <div>
+            <h2 onClick={this.setImageGallery}>IMAGES</h2>
+            {!this.state.isVideosVisible && <Divider />}
+          </div>
+          <div>
+            <h2 onClick={this.setVideoGallery}>VIDEOS</h2>
+            {this.state.isVideosVisible && <Divider />}
+          </div>
+        </Headers>
+        {this.state.isVideosVisible ? (
+          <VideosPage />
+        ) : (
+          <ImagesPage />
+          )}
       </div>
     )
+  }
+  setImageGallery = () => {
+    this.setState ({
+      isVideosVisible: false
+    })
+  }
+
+  setVideoGallery = () => {
+    this.setState ({
+      isVideosVisible: true
+    })
   }
 }
 
