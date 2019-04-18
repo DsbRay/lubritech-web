73d12fa5156b7675318362204aa2562efe1961d1
Duran_Ray <duran@teamgeek.co.za>
2019-04-18 15:02:45 +0200

    go back live

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 // COMPONENTS
 import Header from '../components/navbar'
 import Footer from '../components/footer'
-import ComingSoon from '../components/coming-soon'
+// import ComingSoon from '../components/coming-soon'
 // STYLES
 import '../../node_modules/react-id-swiper/src/styles/css/swiper.css';
 import './index.css'
@@ -18,12 +18,12 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <ComingSoon />
-    {/* <Header />
+    {/* <ComingSoon /> */}
+    <Header />
     <div>
       {children()}
     </div>
-    <Footer /> */}
+    <Footer />
   </div>
 )
 
